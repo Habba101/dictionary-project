d5ee540d2c4249d3252a0ce85eb43ebd0d68f3cc
Habba101 <rahabamotaung@yahoo.com>
2026-08-16 23:58:14 +0200

    Removed alert

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,7 +11,7 @@ export default function Dictionary() {
 
     function search(event) {
         event.preventDefault();
-        alert(`searching for ${keyword} definition...`);
+       
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
         console.log(apiUrl);
         axios.get(apiUrl).then(handleResponse);
